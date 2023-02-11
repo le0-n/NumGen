350966b0f2e74c4de66df1c6f68bf4c0e4a5527b
le0-n <farrux.farxodov2000@gmail.com>
2023-02-11 10:09:54 +0500

    numgenv1.0

---
+++ script.js
@@ -0,0 +1,13 @@
+const min = 100
+const max = 1000
+
+const min_xx1 = 10
+const max_xx1 = 100
+
+const min_xx2 = 10
+const max_xx2 = 100
+
+let xxx = Math.floor(Math.random() * (max - min + 1) + min)
+let xx1 = Math.floor(Math.random() * (max_xx1 - min_xx1 + 1) + min_xx1)
+let xx2 = Math.floor(Math.random() * (max_xx2 - min_xx2 + 1) + min_xx2)
+console.log(`+998 99 ${xxx} ${xx1} ${xx2}`);
