2673f23618a5928c0f57b8067c905c47dfb5fe0d
le0-n <farrux.farxodov2000@gmail.com>
2023-02-12 21:52:29 +0500

    numGenV1.1

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const min = 100
-const max = 1000
+let xxx = Math.floor(Math.random() * 1000)
+let xx1 = Math.floor(Math.random() * 100) 
+let xx2 = Math.floor(Math.random() * 100) 
+let code = [33, 90, 91, 93, 94, 95, 99]
+let randomCode = code[Math.floor(Math.random() * code.length)]
 
-const min_xx1 = 10
-const max_xx1 = 100
+let len_xxx = xxx.toString().length
+let len_xx1 = xx1.toString().length
+let len_xx2 = xx2.toString().length
 
-const min_xx2 = 10
-const max_xx2 = 100
+if (len_xxx == 1) {
+    xxx = '00' + xxx
+} else if (len_xxx == 2) {
+        xxx = '0' + xxx
+}
+if (len_xx1  == 1) {
+    xx1 = '0' + xx1
+}
+if (len_xx2 == 1) {
+    xx2 = '0' + xx2
+}
 
-let xxx = Math.floor(Math.random() * (max - min + 1) + min)
-let xx1 = Math.floor(Math.random() * (max_xx1 - min_xx1 + 1) + min_xx1)
-let xx2 = Math.floor(Math.random() * (max_xx2 - min_xx2 + 1) + min_xx2)
-console.log(`+998 99 ${xxx} ${xx1} ${xx2}`);
+console.log(`+998 ${randomCode} ${xxx} ${xx1} ${xx2}`)
